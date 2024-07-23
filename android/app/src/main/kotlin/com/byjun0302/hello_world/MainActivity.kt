9b46fcadc08cff3d0fa1bd00cf8e5ac624857f4e
sangjun <byjun0302@gmail.com>
2024-07-23 11:33:09 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/byjun0302/hello_world/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.byjun0302.hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // runApp은 플러터 코드를 실행한다.
+  runApp(
+    // MaterialApp 은 항상 최상단에 위치한다.
+    // Scaffold는 바로 아래 위치한다.
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('오늘 하루도 무난하게'),
+        ),
+      ),
+    ),
+  );
+}
