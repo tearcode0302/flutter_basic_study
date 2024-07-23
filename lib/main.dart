f30378cd5eb34b4d290b12a2e441120179faa002
sangjun <byjun0302@gmail.com>
2024-07-23 12:01:05 +0900

    feat: change app background color

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,12 @@ void main() {
     // Scaffold는 바로 아래 위치한다.
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
-          child: Text('오늘 하루도 무난하게'),
+          child: Text(
+            '오늘 하루도 무난하게',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     ),
